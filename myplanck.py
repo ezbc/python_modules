58ba8678aabe6960ab2f6dab3fb3203c82e22735
ezbc <ezbc@astro.wisc.edu>
2014-04-01 16:12:17 -0500

    Created planck data extraction routine

--- myplanck.py
+++ myplanck.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-''' Extracts a region from the Planck data in HEALpix format, and converts to
+''' Extracts a region from the Planck data in HEALPix format, and converts to
 cartesian or similar projection.
 
 Original Author: Dr. Bob Benjamin.
@@ -12,13 +12,29 @@ import numpy as np
 import pyfits as pf
 import healpy
 
-def build_header(header = None, axes = None, reverse_xaxis = True,):
+def build_header(header = None, axes = None, reverse_xaxis = True, field =
+        0):
 
-    # Change format of healpy header to pyfits header
-    #   ...an actually useful format
-    header_edit = pf.Header()
-    for item in header:
-        header_edit.append(item)
+    ''' Builds a header for the extracted Planck image.
+
+    Parameters
+    ----------
+    header : pyfits.Header instance
+        Header from Planck data as a pyfits.Header object instance.
+    axes : tuple, array-like
+        Tuple of axis grids.
+    reverse_xaxis : bool
+        x-axis maximum at the origin?
+    field : int
+        Column to extract from Planck data.
+
+    Returns
+    -------
+    header_new : pyfits.Header instance
+        Header for extracted region from Planck data as a pyfits.Header object
+        instance.
+
+    '''
 
     # Number of axes
     naxis = len(axes)
@@ -36,17 +52,28 @@ def build_header(header = None, axes = None, reverse_xaxis = True,):
     for i in range(naxis):
         header_new.append(('NAXIS%s' % (i+1), len(axes[i])))
 
+    column = field + 1
+
     items = [('BSCALE', 1.0),
              ('BZERO', 0.0),
              ('BLANK', -1),
-             ('BUNIT', header_edit['TUNIT1']),
+             ('BUNIT', header['TUNIT%s' % column]),
              ('TELESCOP', 'Planck'),
              ]
     for item in items:
         header_new.append(item)
 
-    ctypes = ['GLON', 'GLAT', 'VELO-LSR']
+    # characteristics of data type
+    items = [('TYPE', header['TTYPE%s' % column],
+                header.comments['TTYPE%s' % column]),
+             ('FORM', header['TFORM%s' % column],
+                header.comments['TFORM%s' % column]),
+            ]
+    for item in items:
+        header_new.append(item)
 
+    # Axes characteristics
+    ctypes = ['GLON', 'GLAT', 'VELO-LSR']
     for i, axis in enumerate(axes):
         crpix = 0
     	cdelt = axis[1] - axis[0]
@@ -57,8 +84,6 @@ def build_header(header = None, axes = None, reverse_xaxis = True,):
         	crval = axis[-1]
         elif i == 0 and not reverse_xaxis:
     	    crval = axis[0]
-
-        print('crval = %f' % crval)
 
     	items = [('CRPIX%s' % (i+1), crpix),
     	         ('CDELT%s' % (i+1), cdelt),
@@ -72,12 +97,20 @@ def build_header(header = None, axes = None, reverse_xaxis = True,):
              ('BMAJ', 5/3600.),
              ('BMIN', 5/3600.),
              ('COORDSYS', 'GALACTIC'),
-             ('EPOCH', 2000.)]
+             ('EPOCH', 2000.),
+             ('BAD_DATA', header['BAD_DATA'])]
     for item in items:
         header_new.append(item)
 
     # add rest frequency
-    if header_edit['AST-COMP'] in ['CO-TYPE1', 'CO-TYPE2', 'CO-TYPE3']:
+    if header['AST-COMP'] in ['CO-TYPE1', 'CO-TYPE2', 'CO-TYPE3']:
+        if header['AST-COMP'] == 'CO-TYPE2':
+            if field in (0,1,2,3): # then CO J 1-->0
+                rest_freq = 115.2712018e9 # Hz
+            elif field in (4,5,6,7): # then CO J 2-->1
+                rest_freq = 230.5380000e9 # Hz
+            elif field in(8,9,10,11): # then CO J 3-->2
+                rest_freq = 345.796e9 # Hz
     	header_new.append(('RESTFREQ', 115.2712018e9))
 
     return header_new
@@ -160,22 +193,6 @@ def get_planck_filename(data_location = './', data_type = None,
     dust_types = ['Thermal', 'Dust Opacity']
     dust_names = ['dust-commrul', 'DustOpacity']
 
-    ttypeArray = ['TAU353',
-                  'TAU353ERR',
-                  'EBV',
-                  'EBV_ERR',
-                  'T_HF',
-                  'T_HF_ERR',
-                  'BETAHF',
-                  'BETAHFERR']
-    ttypeDescArray = ['Opacity 353GHz',
-                      'Error on opacity',
-                      'E(B-V)',
-                      'Error on E(B-V)',
-                      'T for high freq correction',
-                      'Error on T',
-                      'Beta for high freq correction',
-                      'Error on Beta']
 
     # Determine which file is chosen
     if data_type in CO_types:
@@ -186,8 +203,9 @@ def get_planck_filename(data_location = './', data_type = None,
         if data_type == 'Dust Opacity':
             data_file = 'HFI_CompMap_%s_2048_R1.10.fits' % data_name
             nside = 2048
-        elif data_type == 'Thermal':
-            data_file = 'COM_CompMap_%s_2048_R1.10.fits' % data_name
+        if data_type in thermal_array:
+            data_file = 'COM_CompMap_ThermalDustModel_2048_R1.10.fits' % \
+                data_type
             nside = 2048
     elif data_type in freq_types:
         if data_type in freq_types[:3]:
@@ -198,13 +216,15 @@ def get_planck_filename(data_location = './', data_type = None,
         	nside = 2048
         	data_file = '%s_SkyMap_%s_%s_R1.10_nominal.fits' % \
                     (receiver_type, data_type, nside,)
-
-    data_file = data_location + data_file
+    else:
+        raise LookupError('Invalid data type chosen.')
 
     if not os.path.isfile(data_file):
         raise IOError('No such file: %s' % data_file)
 
-    return data_file, nside
+    data_file = data_location + data_file
+
+    return data_file
 
 def get_data(data_location='./', data_type = 'CO-Type3', longitude_range =
         (152,180), latitude_range = (-25,-3), field = 0, resolution = 0.01,
@@ -220,17 +240,63 @@ def get_data(data_location='./', data_type = 'CO-Type3', longitude_range =
     data_type : str
         Data type to choose from. Options are:
             Narrow-band: 'CO-Type1', 'CO-Type2', 'CO-Type3'
+                'CO-Type1' fields:
+                    0:  12CO J 1-->0 Intensity
+                    1:  12CO J 1-->0 Intensity Error
+                    2:  12CO J 1-->0 Null Test
+                    3:  12CO J 1-->0 Mask
+                    4:  12CO J 2-->1 Intensity
+                    5:  12CO J 2-->1 Intensity Error
+                    6:  12CO J 2-->1 Null Test
+                    7:  12CO J 2-->1 Mask
+                    8:  12CO J 3-->2 Intensity
+                    9:  12CO J 3-->2 Intensity Error
+                    10: 12CO J 3-->2 Null Test
+                    11: 12CO J 3-->2 Mask
+                'CO-Type2' fields:
+                    0:  12CO J 1-->0 Intensity
+                    1:  12CO J 1-->0 Intensity Error
+                    2:  12CO J 1-->0 Null Test
+                    3:  12CO J 1-->0 Mask
+                    4:  12CO J 2-->1 Intensity
+                    5:  12CO J 2-->1 Intensity Error
+                    6:  12CO J 2-->1 Null Test
+                    7:  12CO J 2-->1 Mask
+                'CO-Type3' fields:
+                    0:  12CO Intensity
+                    1:  12CO Intensity Error
+                    2:  12CO Null Test
+                    3:  12CO Mask
+
             Broad-band (GHz): '030', '044', '070', '100', '143', '217', '353',
-                        '545', '857'
-            Processed data products: 'Thermal', 'Dust Opacity', 'TAU353',
-                'TAU353ERR', 'EBV', 'EBV_ERR', 'T_HF', 'T_HF_ERR', 'BETAHF',
-                'BETAHFERR'
+                              '545', '857'
+                Broad-band fields:
+                    0: I stokes
+                    1: Hits
+                    2: II_cov
+
+            Processed data products: 'Dust Opacity', 'Thermal'
+                'Dust Opacity' fields:
+                    0: Intensity
+                    1: Intensity standard deviation
+                    2: Intensity ??
+                    3: Intensity ??
+                'Thermal' dust model fields:
+                    0: 'Opacity 353GHz',
+                    1: 'Error on opacity',
+                    2: 'E(B-V)',
+                    3: 'Error on E(B-V)',
+                    4: 'T for high freq correction',
+                    5: 'Error on T',
+                    6: 'Beta for high freq correction',
+                    7: 'Error on Beta']
+
     longitude : array-like
         Lower and upper longitude.
     latitude : array-like
         Lower and upper latitude.
     field : int
-        Field in data.
+        Field in data type.
     resolution : float
         Pixel resolution in arcseconds.
     cut_last_pixel : bool
@@ -252,7 +318,7 @@ def get_data(data_location='./', data_type = 'CO-Type3', longitude_range =
     '''
 
     # Get the filename
-    filename, nside = get_planck_filename(data_type = data_type, data_location =
+    filename = get_planck_filename(data_type = data_type, data_location =
             data_location)
 
     if verbose:
@@ -261,6 +327,15 @@ def get_data(data_location='./', data_type = 'CO-Type3', longitude_range =
     # Read the map using healpy library
     map_data = healpy.read_map(filename, field = field, h = True)
     map_raw, header_raw = map_data[0], map_data[1]
+
+    # Change format of healpy header to pyfits header
+    #   ...an actually useful format
+    header_pf = pf.Header()
+    for item in header_raw:
+        header_pf.append(item)
+
+    # Get nside from HEALPix format, acts as resolution of HEALPix image
+    nside = header_pf['NSIDE']
 
     # Set up longitude / latitude grid for extracting the region
     longitude_res, latitude_res = resolution, resolution
@@ -305,8 +380,9 @@ def get_data(data_location='./', data_type = 'CO-Type3', longitude_range =
         map_region = map_region.T[::, ::-1]
 
     if return_header:
-    	header_region = build_header(header = header_raw, axes =
-    	        (longitude_axis, latitude_axis), reverse_xaxis = reverse_xaxis)
+    	header_region = build_header(header = header_pf, axes =
+    	        (longitude_axis, latitude_axis), reverse_xaxis = reverse_xaxis,
+    	        field = field)
         return map_region, header_region
     else:
         return map_region
@@ -323,15 +399,18 @@ def main():
         pf.writeto('/d/bip3/ezbc/taurus/data/planck/taurus_planck_region.fits',
                 data, header = header, clobber = True, output_verify = 'fix')
 
-    # Extract the data
-    (data, header) = get_data(data_location=data_location, data_type='CO-Type3',
-            longitude_range = (145,165), latitude_range=(-30,-5), field=0,
-            resolution = 0.01, cut_last_pixel = False, verbose = True)
+    if True:
+        # Extract the data
+        (data, header) = get_data(data_location=data_location,
+                data_type='CO-Type3', longitude_range = (145,165),
+                latitude_range=(-30,-5), field=0, resolution = 0.01,
+                cut_last_pixel = False, verbose = True)
 
-    # Write the data to FITS format
-    pf.writeto('/d/bip3/ezbc/perseus/data/planck/perseus_planck_region.fits',
-            data, header = header, clobber = True, output_verify = 'fix')
+        print(header)
 
+        # Write the data to FITS format
+        pf.writeto('/d/bip3/ezbc/perseus/data/planck/perseus_planck_region.fits',
+                data, header = header, clobber = True, output_verify = 'fix')
 
 if __name__ == '__main__':
 	main()
