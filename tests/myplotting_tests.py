7d39bad570a84ff41bef662df5fbc5d185eeb1ae
ezbc <ezbc@astro.wisc.edu>
2016-04-27 14:43:15 -0500

    added test for log plotting, incomplete

--- tests/myplotting_tests.py
+++ tests/myplotting_tests.py
@@ -88,6 +88,118 @@ if 0:
         '''
 
 
+
+    def test_triangle_dist():
+
+        import numpy as np
+        import myplotting as myplt
+
+        dists = np.zeros((4,5,6))
+        dists[0,1,2] = 0.1
+        dists[1,1,2] = 0.1
+        dists[2,1,2] = 0.1
+        dists[3,4,4] = 0.1
+        dists[3,3,4] = 0.1
+
+        myplt.corner_plot(dists, filename='test_plots/test_corner_plot.png')
+
+if 1:
+    def test_scatter_contour_log():
+
+        import myimage_analysis as myia
+
+        # Parameters
+        # ----------
+        levels = (0.99, 0.985, 0.7)
+        levels = (0.999, 0.998, 0.96, 0.86, 0.58,)
+        levels = 7
+        levels = np.logspace(np.log10(0.995), np.log10(0.50), 5)
+        log_counts = 0
+        limits = [1, 10, -3, 30]
+        limits = None
+
+        x = np.linspace(1, 100, 10000)
+        y = x**3 + np.random.normal(10, size=10000) + 100
+        y_real = x**3 + 100
+
+        x_nonans, y_nonans = myia.mask_nans((x,y))
+        scale = ['linear', 'log']
+
+        fig, ax = plt.subplots()
+
+        if limits is None:
+            xmin = np.min(x_nonans)
+            ymin = np.min(y_nonans)
+            xmax = np.max(x_nonans)
+            ymax = np.max(y_nonans)
+            if scale[0] == 'linear':
+                xscalar = 0.25 * xmax
+                xlims = xmin - xscalar, xmax + xscalar
+            elif scale[0] == 'log':
+                xscalar = 0.25
+                xlims = xmin * xscalar, xmax / xscalar
+            if scale[1] == 'linear':
+                yscalar = 0.25 * ymax
+                ylims = ymin - yscalar, ymax + yscalar
+            elif scale[1] == 'log':
+                yscalar = 0.25
+                ylims = ymin * yscalar, ymax / yscalar
+
+            limits = [xlims[0], xlims[1], ylims[0], ylims[1]]
+
+        if 1:
+            bins = [30, np.logspace(np.log10(limits[2]),
+                                    np.log10(limits[3]),
+                                    30),
+                    ]
+        else:
+            bins = 40
+
+        print bins
+
+        contour_range = ((limits[0], limits[1]),
+                         (limits[2], limits[3]))
+
+        cmap = myplt.truncate_colormap(plt.cm.binary, 0.2, 1, 1000)
+
+        ax.set_xscale(scale[0], nonposx = 'clip')
+        ax.set_yscale(scale[1], nonposy = 'clip')
+
+        l1 = myplt.scatter_contour(x_nonans.ravel(),
+                             y_nonans.ravel(),
+                             threshold=3,
+                             log_counts=log_counts,
+                             levels=levels,
+                             ax=ax,
+                             extent=limits,
+                             histogram2d_args=dict(bins=bins,
+                                                   range=contour_range,
+                                                   ),
+                             plot_args=dict(marker='o',
+                                            linestyle='none',
+                                            color='black',
+                                            alpha=0.3,
+                                            markersize=2),
+                             contour_args=dict(
+                                               #cmap=plt.cm.binary,
+                                               cmap=cmap,
+                                               #cmap=cmap,
+                                               ),
+                             )
+
+        ax.plot(x, y_real, color='r', alpha=0.5, linewidth=2)
+
+        ax.set_xlim(limits[0],limits[1])
+        ax.set_ylim(limits[2],limits[3])
+
+        # Adjust asthetics
+        ax.set_xlabel(r'x')
+        ax.set_ylabel(r'y')
+        #ax.set_title(core_names[i])
+        ax.legend(loc='lower right')
+
+        plt.savefig('test_plots/test_scatter_contour_log.png')
+
     def test_scatter_contour():
 
         from astropy.io import fits
@@ -180,20 +292,4 @@ if 0:
         ax.legend(loc='lower right')
 
         plt.savefig('test_plots/test_scatter_contour.png')
-
-
-def test_triangle_dist():
-
-    import numpy as np
-    import myplotting as myplt
-
-    dists = np.zeros((4,5,6))
-    dists[0,1,2] = 0.1
-    dists[1,1,2] = 0.1
-    dists[2,1,2] = 0.1
-    dists[3,4,4] = 0.1
-    dists[3,3,4] = 0.1
-
-    myplt.corner_plot(dists, filename='test_plots/test_corner_plot.png')
-
 
